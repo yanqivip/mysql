3d0454861fa2fa1daaa5093a0edc7efea4c70bb1
yanqi <yanqi_vip@yeah.net>
2021-08-05 19:47:29 +0800

    mysql

---
+++ mysql.jdbc/src/com/yanqi/jdbc05/JdbcLogin01.java
@@ -0,0 +1,52 @@
+package com.yanqi.jdbc05;
+
+        import com.yanqi.jdbc05.JdbcUtils;
+
+        import java.sql.Connection;
+        import java.sql.ResultSet;
+        import java.sql.SQLException;
+        import java.sql.Statement;
+        import java.util.Scanner;
+
+public class JdbcLogin01 {
+
+    /**
+     * 用户登录案例
+     * @param args
+     */
+    public static void main(String[] args) throws SQLException {
+
+        //1.获取连接
+        Connection con = JdbcUtils.getConnection();
+
+        //2.获取Statement对象
+        Statement statement = con.createStatement();
+
+        //3.获取用户输入的用户名和密码
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入用户名: ");
+        String name = sc.nextLine();
+
+        System.out.println("请输入密码: ");
+        String pass = sc.nextLine();
+
+        //4.拼接SQL语句
+        String sql = "select * from jdbc_user where username = '" + name + "' and password = '" + pass +"'";
+        System.out.println(sql);
+
+        //5.执行查询 获取结果集对象
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        //6.处理结果集
+        if(resultSet.next()){
+
+            System.out.println("登录成功! 欢迎您: " + name);
+        }else{
+
+            System.out.println("登录失败! ");
+        }
+
+        //7.关闭流
+        JdbcUtils.close(con,statement,resultSet);
+    }
+}
